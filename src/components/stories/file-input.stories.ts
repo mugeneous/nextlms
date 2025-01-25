a0607346b51a7f9cad93058530fd98d0e0f3cb15
mugeneous <unitonework@gmail.com>
2025-01-25 12:49:52 +0700

    feat: add file input and file input stories components

---
+++ src/components/stories/file-input.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { FileInput } from "@/components/file-input";
+
+const meta = {
+  title: "File Input",
+  component: FileInput,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    children: {
+      control: "text",
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof FileInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const InputFile: Story = {
+  args: {
+    placeholder: "Choose file",
+  },
+};
