1a557aaabdcadc58b53f30b30251fdabfe767cbf
mugeneous <unitonework@gmail.com>
2025-01-25 13:03:57 +0700

    feat: add button and button stories components

--- src/components/stories/button.stories.ts
+++ src/components/stories/button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from "../Button";
+import { Button } from "@/components/button";
 
 const meta = {
   title: "Button",
@@ -8,10 +8,41 @@ const meta = {
   parameters: {
     layout: "centered",
   },
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+      options: ["primary", "secondary", "danger"],
+    },
+    size: {
+      control: { type: "select" },
+      options: ["lg", "md", "sm"],
+    },
+    children: {
+      control: "text",
+    },
+  },
   tags: ["autodocs"],
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const ButtonPrimary: Story = {};
+export const ButtonPrimary: Story = {
+  args: {
+    variant: "primary",
+    children: "create course",
+  },
+};
+
+export const ButtonSecondary: Story = {
+  args: {
+    variant: "secondary",
+    children: "create course",
+  },
+};
+export const ButtonDanger: Story = {
+  args: {
+    variant: "danger",
+    children: "create course",
+  },
+};
