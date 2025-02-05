b4cea3c8b980dc5e01f9bf0bdcbfc92260bda49e
mugeneous <unitonework@gmail.com>
2025-02-05 22:50:45 +0700

    feat: configure images.remotePatterns for external image support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-337062e00c0f4c9abd71886cf71370cf.r2.dev",
+        pathname: "/**",
+        port: "",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
